b59bf2993183925941adca2cdb08682fdc7d4cbb
Artem Nesin <nesin.a@rocket.red>
2024-07-21 11:09:25 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Пойдем сегодня на свидание?</h1>
+    <div class="button-box">
+      <button>Да</button>
+      <button>Конечно да</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.button-box {
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+}
+</style>
